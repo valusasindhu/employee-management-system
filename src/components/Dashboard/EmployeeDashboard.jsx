467afefc2169918829446471da7eaf60cf7f3e96
valusasindhu <valusasindhu@gmail.com>
2025-05-08 16:15:21 +0530

    commit

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers.jsx'
+import TaskList from '../TaskList/TaskList.jsx'
+
+
+const EmployeeDashboard = (props) => {
+
+    return (
+        <div className='p-10 bg-[#1C1C1C] h-screen'>
+
+            <Header changeUser={props.changeUser} data={props.data} />
+            <TaskListNumbers data={props.data} />
+            <TaskList data={props.data} />
+
+        </div>
+    )
+}
+
+export default EmployeeDashboard
